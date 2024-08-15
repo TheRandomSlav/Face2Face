3f571457a29eb0f096a673c61e221b699a0fce94
Viacheslav Planida <slavaplanida@yandex.ru>
2024-08-15 14:51:26 +0700

    Сделал БД

--- BigBrother/main/models.py
+++ BigBrother/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
+class Persona(models.Model):
+    name = models.CharField(max_length=255)
+    embedding = models.BinaryField()
+
+    def __str__(self):
+        return self.name
 # Create your models here.
+
+
+class FImage(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='imag/')
+    def __str__(self):
+        return self.name
