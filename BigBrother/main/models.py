80e3fb9eac88c98d2a9d870fe5b00204e195582c
Viacheslav Planida <slavaplanida@yandex.ru>
2024-08-22 19:18:44 +0700

    В БД заменил BLOB на JSON

--- BigBrother/main/models.py
+++ BigBrother/main/models.py
@@ -3,7 +3,12 @@ from django.db import models
 
 class Persona(models.Model):
     name = models.CharField(max_length=255)
-    embedding = models.BinaryField()
+    embedding = models.JSONField()
+
+    def save(self, *args, **kwargs):
+        # Преобразуем вектор признаков в формат JSON
+        self.embedding = json.dumps(self.embedding)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
