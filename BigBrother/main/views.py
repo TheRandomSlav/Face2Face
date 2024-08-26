f38fe4ad72d4a1b29cca2aa3267877aa82252a85
Viacheslav Planida <slavaplanida@yandex.ru>
2024-08-26 17:46:05 +0700

    Сделал форму отправки видео, начал разработку представлений

--- BigBrother/main/views.py
+++ BigBrother/main/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+def is_video(file):
+    try:
+        clip = mp.VideoFileClip(file)
+        return True
+    except IOError:
+        return False
+
 def index(request):
+    if request.method == 'POST' :
+        form = FileUploadForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            file = request.FILES['file']
+
+            if is_video(file.temporary_file_path()):
+                # Файл является видеозаписью
+                return render(request, 'main/index.html') # Обработай и добавь данные
+
+            else:
+                # Файл не является видеозаписью
+                return render(request, 'main/index.html')
+
     return render(request, 'main/index.html')
